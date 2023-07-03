ab91ae2bc4e2791ebbfd3a489e1bb475766358ff
Szymon Wysogląd <wysogladszymon@gmail.com>
2023-07-03 17:06:02 +0200

    Concert Tickets

--- CSES/Sorting and Searching/Concert Tickets.cpp
+++ CSES/Sorting and Searching/Concert Tickets.cpp
@@ -6,35 +6,24 @@ typedef long long ll;
 int main()
 {
     std::ios_base::sync_with_stdio(0); std::cin.tie(0);
-
-    int n, m, k;
-    std::cin >> n >> m;
-    std::vector<int> tickets;
-
-    for (int i = 0; i < n; i++)
+    int n, m, a;
+    std::cin >> n >>m;
+    std::multiset<int> s;
+    for (int i = 0; i < n;++i)
     {
-        std::cin >> k;
-        tickets.push_back(k);
+        std::cin >> a;
+        s.insert(a);
     }
-    std::sort(tickets.begin(), tickets.end());
-
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m; ++i)
     {
-        std::cin >> k;
-        int j = 0;
-        int z = 0;
-        while(k >= tickets[j] && j < n)
+        std::cin >> a;
+        auto it = s.upper_bound(a);
+        if(it == s.begin())
+            std::cout << -1 << std::endl;
+        else
         {
-            if(tickets[j] > 0)
-            {
-                z = j; 
-            }
-            ++j;
+            --it;
+            std::cout << *it << std::endl;
         }
-        /*std::cout << "wektor to: ";
-        for(auto a : tickets)
-            std::cout << a<< " ";
-        */std::cout << tickets[z]<<std::endl;
-        tickets[z] = -1; //chosen
     }
 }
